f923c9ab83544af757a5dd654f963299da927401
Wasiq Kashkari <w.kashkari@outlook.com>
2021-01-04 22:03:42 +0100

    Add default redirect so you actually see something

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
+  Redirect
 } from "react-router-dom";
 
 import { HorseList } from './features/horseList/HorseList';
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/" exact render={() => <Redirect to="/horses/page/1"/>} />
           <Route path="/horses/page/:pageNum" exact component={HorseList}/>
         </Switch>
       </Router>
