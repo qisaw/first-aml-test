af393c53e5bfe5778d9a3512d8729001ba602b71
Wasiq Kashkari <w.kashkari@outlook.com>
2021-01-04 22:22:43 +0100

    Remove counter example

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import horseListReducer from '../features/horseList/counterSlice'
 
 export const setUpStore = () => configureStore({
   reducer: {
-    counter: counterReducer,
     horseList: horseListReducer,
   },
 })
